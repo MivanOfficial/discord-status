ef5bfe5d615902f574bd55386e704d4415b31456
MivanOfficial <56939387+ThePersigg@users.noreply.github.com>
2019-11-07 12:12:55 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+from discord import Game
+
+bot = commands.Bot(command_prefix= "!")
+
+@bot.event
+async def on_ready():
+    print("Status is online. Made by MivanOfficial💓")
+    print("Discord Username: {}".format(bot.user.name))
+    print("Discord UserID: {}".format(bot.user.id))
+    await bot.change_presence(activity=Game(name="💓Ваш статус", type="WATCHING")) 
+    #прочитайте файл statuses.json там написаны все статусы.
+
+bot.run("ваш токен", bot=false)
