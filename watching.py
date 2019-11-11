5af39ca11f7ee3b0213f1eb764d75eb97a85a732
Mivan <56939387+MivanOfficial@users.noreply.github.com>
2019-11-11 22:15:21 +0500

    Create watching.py

---
+++ watching.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+from discord import Game
+
+bot = commands.Bot(command_prefix= "!")
+
+@bot.event
+async def on_ready():
+    print("Status is online. Made by MivanOfficial💓")
+    print("Discord Username: {}".format(bot.user.name))
+    print("Discord UserID: {}".format(bot.user.id))
+    
+    statusTEXT = config.statusTEXT
+    activity = discord.Activity(name=stat, type=discord.ActivityType.watching)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+bot.run(config.token, bot=False)
