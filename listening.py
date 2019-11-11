f5058efe88ee1c4890ffdee6e8c5f8a77c57de5c
Mivan <56939387+MivanOfficial@users.noreply.github.com>
2019-11-11 22:14:10 +0500

    Create listening.py

---
+++ listening.py
@@ -0,0 +1,18 @@
+import discord
+import config
+from discord.ext import commands
+from discord import Game
+
+bot = commands.Bot(command_prefix= "!")
+
+@bot.event
+async def on_ready():
+    print("Status is online. Made by MivanOfficial💓")
+    print("Discord Username: {}".format(bot.user.name))
+    print("Discord UserID: {}".format(bot.user.id))
+    
+    statusTEXT = config.statusTEXT
+    activity = discord.Activity(name=statusTEXT, type=discord.ActivityType.listening)
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+bot.run(config.token, bot=False)
